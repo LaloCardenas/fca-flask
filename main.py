d393ff97aa85279c7d3fd08c3b57ebcdc35ac23f
Lyd6065 <lalo020404@gmail.com>
2025-03-26 09:40:45 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,27 @@ import os
 
 app = Flask(__name__)
 
+class Contact:
+    def __init__(self, name, phone):
+        self.name = name
+        self.phone = phone
+
 
 @app.route('/')
 def index():
-    return jsonify({"Cristian Cardoso": "+525523457687", "Santiago Amezcua": "+525567879687", "Mario Centeno": "+5234543321", "Gerardo Cornejo": "+52455565465"})
+    contacts = []
+    
+    cris = Contact("Cristian Cardoso", "+525523457687")
+    santi = Contact("Santiago Amezcua", "+525567879687")
+    mario = Contact("Mario Centeno", "+5234543321")
+    gerry = Contact("Gerardo Cornejo", "+52455565465")
+
+    contacts.append(cris)
+    contacts.append(santi)
+    contacts.append(mario)
+    contacts.append(gerry)
+    
+    return jsonify(contacts)
 
 
 if __name__ == '__main__':
